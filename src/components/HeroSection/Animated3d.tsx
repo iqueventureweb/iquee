6ae935535247330704043af7e8fc327766d531b3
AJMAL <piacademyweb1@gmail.com>
2025-09-17 18:00:17 +0100

    updating the spline

--- src/components/HeroSection/Animated3d.tsx
+++ src/components/HeroSection/Animated3d.tsx
@@ -15,10 +15,20 @@ export default function Animated3d() {
 
   return (
     <div className="absolute inset-0 z-0 bg-white">
+      {/* Mobile Spline - visible only below md breakpoint */}
       <Spline
         scene="https://prod.spline.design/Fli8BeD7SGhjHj4C/scene.splinecode"
         onSplineScroll={handleSplineScroll}
         style={{ backgroundColor: "white" }}
+        className="block md:hidden"
+      />
+
+      {/* Desktop Spline - visible from md breakpoint and above */}
+      <Spline
+        scene="https://prod.spline.design/zvMR1KIPEox3SNRu/scene.splinecode"
+        onSplineScroll={handleSplineScroll}
+        style={{ backgroundColor: "white" }}
+        className="hidden md:block"
       />
     </div>
   );
